c35bf857a3fdcce520030273f6a4f81ac32d9c8a
Ruban Daniil <danya.ruban.2005@mail.ru>
2024-12-02 17:58:43 +0300

    Fix colors

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -3,9 +3,33 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
-import { COLORS } from "@/constants/colors";
+//import { COLORS } from "@/constants/colors";
 import { useChoppTheme } from "@/shared/context/chopp-theme-context";
 import { LIGHT_THEME } from "@/theme";
+
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+const COLORS = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+  },
+  // light: LIGHT_THEME.colors,
+  // dark: DARK_THEME.colors
+};
 
 //TODO: отрефакторить, выяснить нужен ли вообще этот компонент
 export function useThemeColor(
